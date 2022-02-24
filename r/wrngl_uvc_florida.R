245c97e29d4e34d58a45e11519171c92bfda910f
JayMox <jerry.moxley@gmail.com>
2022-02-24 12:45:27 -0800

    taxa lkup tbl in fla raw folder

--- r/wrngl_uvc_florida.R
+++ r/wrngl_uvc_florida.R
@@ -1,3 +1,11 @@
 #wrngl of florida data
 #JHMoxley Feb 22
 
+source(here('r', 'fxn_library.R'))
+ctry <- "florida"
+message(paste("sourcing raw uvc counts from", ctry))
+# src <- c('src_Fish_Ab_Rosario.csv',
+#          'src_Fish_Ab_San_Bernardo.csv')
+n.src <- length(unique(src))
+message(paste(n.src,"src files found in stitch"))
+message(paste(src, "frm", ctry))
