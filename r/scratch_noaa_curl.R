79c39ab2dccd77ab8f121a42eb72fb0f884bbf0c
JayMox <jerry.moxley@gmail.com>
2022-02-24 14:48:38 -0800

    interp data fields for fla

--- r/scratch_noaa_curl.R
+++ r/scratch_noaa_curl.R
@@ -6,3 +6,24 @@ devtools::install_github('jeremiaheb/rvc')
 require(rvc)
 #pkg dependency ages out macOS :(
 #attempting to use docker images
+
+
+ctry <- "florida"
+src <- c('x_flaFish_2016.csv')
+
+sc <- read_csv(here('data/stitch',  src[j])) %>%
+   janitor::clean_names()
+
+#look at structure of sampling units
+sc %>% group_by(primary_sample_unit) %>% 
+  select(primary_sample_unit, station_nr, zone_nr,
+         subregion_nr, species_cd,
+         month, day) %>% 
+  summarize(n.station = length(unique(station_nr)),
+            n.zone = length(unique(zone_nr)),
+            n.subreg = length(unique(subregion_nr)),
+            n.spp = length(unique(species_cd)),
+            n.mos = length(unique(month)),
+            n.day = length(unique(day)))
+#2 secondary stations per priamry w/ a count of individuals
+#
--- r/wrngl_uvc_florida.R
+++ r/wrngl_uvc_florida.R
@@ -1,11 +1,34 @@
 #wrngl of florida data
 #JHMoxley Feb 22
-
+library(tidyverse)
+library(here)
 source(here('r', 'fxn_library.R'))
 ctry <- "florida"
 message(paste("sourcing raw uvc counts from", ctry))
-# src <- c('src_Fish_Ab_Rosario.csv',
-#          'src_Fish_Ab_San_Bernardo.csv')
+src <- c('x_flaFish_2016.csv')
 n.src <- length(unique(src))
 message(paste(n.src,"src files found in stitch"))
 message(paste(src, "frm", ctry))
+
+#sweep in src data
+raw <- NULL
+for(j in 1:n.src){
+  dat <- read_csv(here('data/stitch',  src[j])) %>%
+    janitor::clean_names() %>% 
+    mutate(country = as.factor(ctry), 
+           src = as.factor(src[j]), 
+    ) %>% 
+    select(transect = station_nr, #or should this 
+           year, )
+    mutate(
+      especie_des = 
+        ifelse(especie == "Ccru", 
+               "Cephalopholis cruentata",
+               especie_des)
+      #deal w/ species w/ funky encoding error
+    ) %>% 
+    select(-longitud_des)
+  
+  raw <- bind_rows(raw, dat)
+  message(paste("dat from", src[j], " inputted"))
+}
