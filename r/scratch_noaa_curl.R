400a09e3da22c3dc0125e9741f5df541fd98a89a
JayMox <jerry.moxley@gmail.com>
2022-02-24 14:01:27 -0800

    looking into docker bc of dependency issues

---
+++ r/scratch_noaa_curl.R
@@ -0,0 +1,8 @@
+#scratch script exploring rvc tools
+#see here: https://github.com/jeremiaheb/rvc
+
+library(devtools)
+devtools::install_github('jeremiaheb/rvc')
+require(rvc)
+#pkg dependency ages out macOS :(
+#attempting to use docker images
